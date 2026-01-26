7dfb12a2a2cec1a31f97fed2a1d7ca79f0988317
BelGraDev <carlosgranadosbeltran@gmail.com>
2026-01-26 21:14:38 +0100

    Controller status is now a dataclass

--- Controller/controllerStatus.py
+++ Controller/controllerStatus.py
@@ -1,15 +1,29 @@
 from Board.logicManager import LogicManager
 from Board.Pieces import Piece
+from dataclasses import dataclass, field
+from random import randint
+from typing import Optional
 
+@dataclass
 class ControllerStatus:
 
-    def __init__(self, logic_manager: LogicManager):
-        self.cell_highlighted: str | None = None
-        self.pawn_ascending = False
-        self.ascension_pieces: list[Piece] = []
-        self.possible_moves_cells: list[str] = []
+    logic_manager: LogicManager
+    cell_highlighted: Optional[str] = None
+    pawn_ascending = False
+    player_turn: str = field(init=False)
+    ai_turn: str = field(init=False)
+    ascension_pieces: list[Piece] = field(default_factory=list[Piece])
+    possible_moves_cells: list[str] = field(default_factory=list[str])
 
-        self.logic_manager = logic_manager
+
+    def __post_init__(self):
+        self.player_turn, self.ai_turn = self._calculate_player_and_ai_turn()
+
+
+    def _calculate_player_and_ai_turn(self) -> tuple[str, str]:
+        player_turn = 'w' if randint(0, 1) == 0 else 'b'
+        ai_turn = 'w' if player_turn == 'b' else 'b'
+        return player_turn, ai_turn
 
 
     def highlight_cell(self, cell_name: str) -> None:
