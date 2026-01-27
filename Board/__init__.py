57d293015fc41a667c2277c8892129e8cd29f0f6
BelGraDev <carlosgranadosbeltran@gmail.com>
2026-01-27 11:15:25 +0100

    Changed folder structure so that the logic is more organized in one place

---
+++ Board/__init__.py
@@ -0,0 +1,5 @@
+
+from .boardCells import BoardCells, Position                #type: ignore
+from .pieceColor import PieceColor                          #type: ignore
+from .BoardStatus import  BoardStatus                       #type: ignore
+from .logicManager import LogicManager                      #type: ignore
